fc02cca9e94440a22eba3c9460e64fe7b9389fe0
Elias SACI <esaci@student.42.fr>
2022-03-19 10:42:09 +0100

    auto

--- srcs/main.c
+++ srcs/main.c
@@ -5,9 +5,14 @@ int main( void ){
 	WINDOW *stdscr;
 
 	stdscr = initscr();				//Initialise la structure WINDOW et autres paramètres 
-	keypad(stdscr, TRUE);
+	// keypad(stdscr, TRUE);
 	getmaxyx(stdscr, h, w);
 	printf("h vaut %d et w %d\n", h, w);
+	for(int i = 0; i < h; i++)
+	{
+		mvprintw(1 , i, "-");
+		// refresh();
+	}
 	// WINDOW * win = newwin(nlines, ncols, y0, x0);
 	// printw("Hello World");	//Écrit Hello World à l'endroit où le curseur logique est positionné
 	// refresh();				//Rafraîchit la fenêtre courante afin de voir le message apparaître
